5c5d285bcd7ab6c5d72d790adb6d24557b4f190e
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2019-12-13 08:16:32 -0800

    fixed display answer problem, mostly with help from TL

--- myredux-app/src/components/Trivia.js
+++ myredux-app/src/components/Trivia.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
 import { getTrivia } from '../actions';
 import Loader from 'react-loader-spinner';
 
 const Trivia = props => {
+    const [revealed, setRevealed] = useState(false)
+    const toggle = e => setRevealed(!revealed)
     return(
         <div>
             <h1>This is JEOPARDY!</h1>
@@ -16,7 +18,15 @@ const Trivia = props => {
                 width={100}
                 />
             )}
-            {props.trivia && <p>{props.trivia.question}</p>}
+            {props.trivia ? (
+                <>
+                    <p>{props.trivia[0].question}</p> 
+                    <p>{props.trivia[0].category.title}</p> 
+                    <button onClick={toggle}>See Answer</button>
+                    <p className={`answer${revealed ? ' revealed' : ''}`}>{props.trivia[0].answer}</p>
+                </>
+            ) : null}
+
             <button onClick={props.getTrivia}>Get new clue</button>
         </div>
     );
